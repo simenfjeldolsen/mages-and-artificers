d6022a4bdb82ec98c9fb20de97e149e6fb93054f
Simen Fjeld-Olsen <simen.fjeldolsen@gmail.com>
2024-01-15 17:09:29 +0100

    Defines

---
+++ common/defines/NDefines.NCountry.lua
@@ -0,0 +1,21 @@
+-- For Ideas Expanded
+NDefines.NCountry.PS_BUY_IDEA = 400
+NDefines.NCountry.FREE_IDEA_GROUP_COST  = 7	-- Number of ideas per National Idea
+NDefines.NCountry.IDEA_TO_TECH = -0.01
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 12
+NDefines.NCountry.BASE_POSSIBLE_POLICIES = 4
+NDefines.NCountry.MINIMUM_POLICY_TIME = 2
+NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35 --35
+
+-- Innovativeness
+NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 1 -- 2.0
+NDefines.NCountry.INNOVATIVENESS_DAYS_AFTER_FIRST_PICK_VIABLE = 60 --90
+NDefines.NCountry.INNOVATIVENESS_FIRST_REACHED_TECH_LEVEL = 2 -- 4.0
+
+-- Alliance
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
+
+-- Military
+NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 0.67
+NDefines.NMilitary.WARGOAL_MAX_BONUS = 40
+NDefines.NMilitary.BACK_LINE_MORALE_DAMAGE_TAKEN_MODIFIER = 0.0 -- Cannons not retreating?
